327a01fb3b4af32b77ee82abbf33516da5328a73
Jojofrmbk <joenellj2@nycstudents.net>
2025-12-02 12:00:16 -0500

    Update bm0_madlibs.py

--- bm0_madlibs.py
+++ bm0_madlibs.py
@@ -1,3 +1,9 @@
 print('Enter your name:')
 name=input()
 print(f"Hello {name}")
+
+verb= input(f'Hello Pleaes enter a verb')
+noun= input(f'Hello Please enter a noun')
+adjective= input(f'Hello Please enter an adjective')
+
+print(verb)
